4804d67c0dff4145066e6f6573d7572c256a5c43
Renan Oliveira <renansmoliveira@gmail.com>
2025-11-19 22:40:20 -0300

    fix: colors

--- utils/calculateFieldColor.ts
+++ utils/calculateFieldColor.ts
@@ -1,6 +1,5 @@
 export const calculateFieldColor = (value: number, thresholds: number[]) => {
   if (value < thresholds[0]) return "text-red-600";
-  if (value <= thresholds[1]) return "text-yellow-600";
-  if (value <= thresholds[2]) return "text-blue-600";
+  if (value > thresholds[0] && value <= thresholds[1]) return "text-yellow-600";
   return "text-green-600";
 };
