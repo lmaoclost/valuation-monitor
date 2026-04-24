af3af7b712aa18a5623a65aee941275a93c6c9fc
Renan Oliveira <renansmoliveira@gmail.com>
2026-04-24 11:02:32 -0300

    chore: add license description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,19 @@
+// Valuation Monitor - A fundamentalist radar for filtering Brazilian stocks
+// Copyright (C) 2026  Renan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import type { Metadata } from "next";
 import { Playfair_Display, Lora, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
@@ -7,12 +23,10 @@ const playfair = Playfair_Display({
   subsets: ["latin"],
   variable: "--font-display",
 });
-
 const lora = Lora({
   subsets: ["latin"],
   variable: "--font-body",
 });
-
 const jetbrains = JetBrains_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
